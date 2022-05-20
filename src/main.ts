0dce1fb5034eb98e41c8a7b6dfe894958e0e2de2
Harlan <harlanhai@163.com>
2022-05-20 20:24:51 +0800

    删除测试文件

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from 'vue';
 import 'ant-design-vue/dist/antd.css';
-import Antd from 'ant-design-vue';
-// import { setupStore } from './store'; // 状态管理
+
 import App from './App.vue';
 import router from './routes';
 
 const app = createApp(App);
 app.use(router);
-app.use(Antd);
-// setupStore(app);
 
 app.mount('#app');
